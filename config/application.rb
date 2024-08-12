c3c828b20c3b69f4041b5994d4f0a013c0108ec5
masaengineer <masahiro.track.a.f@gmail.com>
2024-08-12 18:07:00 +0900

    fix: Minitest関連ファイルを削除してRSpecを導入

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,14 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.controller_specs false
+      g.view_specs false
+      g.helper_specs false
+      g.routing_specs false
+    end
   end
 end
