a55b03236e5c16c990f97b106b5fb64ff9fbb299
masaengineer <masahiro.track.a.f@gmail.com>
2024-08-15 21:07:47 +0900

    fix: taskテーブルの旧nameカラムをtitleに変更した影響範囲の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name)
+      params.require(:task).permit(:title, :description, :done, :deadline, :priority)
     end
 end
